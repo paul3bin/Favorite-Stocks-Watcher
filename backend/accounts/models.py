caea6254a72757ddf278f9d1cdd621c2e625b864
Ebin Paul <ebinpaul@outlook.com>
2022-05-09 17:49:03 +0530

    initial commit for accounts app

---
+++ backend/accounts/models.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a new user.
+        """
+        if not email:
+            raise ValueError("Users must have an email address!")
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Creates and saves a new superuser.
+        """
+        user = self.create_superuser(email=email, password=password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom user model class.
+    """
+
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+
+    def __str__(self):
+        return self.email
+
+    objects = UserManager()
