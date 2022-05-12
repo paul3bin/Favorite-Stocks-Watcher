0edf115530cb53354cd3749eae5fa044a2063a93
Ebin Paul <ebinpaul@outlook.com>
2022-05-12 20:15:08 +0530

    refactored imports

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,6 +1,8 @@
 from accounts import views
 from django.urls import path
 
+from accounts import views
+
 app_name = "accounts"
 
 urlpatterns = [
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -6,6 +6,8 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
 
+from accounts.serializers import AuthTokenSerializer, UserSerializer
+
 
 class CreateUserView(generics.CreateAPIView):
     """
