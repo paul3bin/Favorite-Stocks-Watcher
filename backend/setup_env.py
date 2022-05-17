289c2ef0bf790e0a556ad283f4313d89024ba12c
Ebin Paul <ebinpaul@outlook.com>
2022-05-17 12:45:48 +0530

    changes in setup env script to save secrets in .env file

--- backend/setup_env.py
+++ backend/setup_env.py
@@ -17,10 +17,19 @@ def generate_random_string(length: int) -> str:
 
 
 def setup_envionment():
-    os.environ["SECRET_KEY"] = generate_random_string(50)
-    os.environ["ALLOWED_HOSTS"] = "localhost, 127.0.0.1, 192.168.0.103"
-    os.environ["DEBUG"] = "True"
-    os.environ["CORS_ALLOWED_ORIGINS"] = "http://localhost:3000"
+    """
+    creates a new .env file within the directory and saves env secrets in it!
+    """
+    secret_key = generate_random_string(50)
+    values = [
+        f"SECRET_KEY={secret_key}",
+        "ALLOWED_HOSTS=localhost, 127.0.0.1",
+        "DEBUG=True",
+        "CORS_ALLOWED_ORIGINS=http://localhost:3000",
+    ]
+
+    with open(".env", "w+") as f:
+        f.write("\n".join(values))
 
     print("ENVIRONMENT SET!")
 
