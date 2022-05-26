b3124b80d28f1bc301ee6c6aa50c2ad08f6f11bd
Ebin Paul <ebinpaul@outlook.com>
2022-05-26 17:20:42 +0530

    added stocks model class

--- backend/stocks/models.py
+++ backend/stocks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import User
+
+
+class Stocks(models.Model):
+    """
+    Model class for user stocks.
+    """
+
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    stock_symbol = models.CharField(max_length=6, unique=True)
+    company_name = models.CharField(max_length=250)
+    added_on = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self):
+        return self.stock_symbol
