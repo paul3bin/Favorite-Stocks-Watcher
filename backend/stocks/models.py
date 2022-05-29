e6b024bb8e291d2785233a014426a740f7b208cf
Ebin Paul <ebinpaul@outlook.com>
2022-05-29 12:37:35 +0530

    added tests for testing stocks model and changes in stocks model class

--- backend/stocks/models.py
+++ backend/stocks/models.py
@@ -8,7 +8,7 @@ class Stocks(models.Model):
     Model class for user stocks.
     """
 
-    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=False, null=False)
     stock_symbol = models.CharField(max_length=6, unique=True)
     company_name = models.CharField(max_length=250)
     added_on = models.DateTimeField(auto_now_add=True, editable=False)
--- backend/stocks/tests.py
+++ backend/stocks/tests.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+from stocks import models
+
+
+def sample_user(email="test@gmail.com", password="testpassword"):
+    # create a sample user
+    return get_user_model().objects.create_user(email, password)
+
+
+class StockModelTest(TestCase):
+    """
+    Test cases for stocks model.
+    """
+
+    def test_create_stock_with_valid_user(self):
+        stock = models.Stocks.objects.create(
+            user=sample_user(), stock_symbol="AAPL", company_name="Apple Inc."
+        )
+
+        self.assertEqual(str(stock), stock.stock_symbol)
