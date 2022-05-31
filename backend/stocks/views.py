f88e28078a1c5680308795b9a57d33e6500565c6
Ebin Paul <ebinpaul@outlook.com>
2022-05-31 11:41:16 +0530

    added methods to create, list and retrieve stocks

--- backend/stocks/views.py
+++ backend/stocks/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from rest_framework import status, viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from stocks import models, serializers
+
+
+class StocksViewSet(viewsets.ModelViewSet):
+    queryset = models.Stocks.objects.all()
+    serializer_class = serializers.StocksSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def create(self, request):
+        """
+        method to create/add a new stock and its details.
+        """
+        serializer = serializers.StocksSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+    def list(self, request):
+        """
+        method to list all the stock details added by the user.
+        """
+        stocks = models.Stocks.objects.all().filter(user=request.user)
+        serializer = serializers.StocksSerializer(stocks, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+        """
+        retrieve the details of individual stock
+        """
+        stock = models.Stocks.objects.get(id=pk)
+        serializer = serializers.StocksSerializer(instance=stock, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
