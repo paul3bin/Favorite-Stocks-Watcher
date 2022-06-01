47932537a26c40d537af1a0e6827b6de3f7bf61b
Ebin Paul <ebinpaul@outlook.com>
2022-06-01 12:12:05 +0530

    fix for retrieve stocks api

--- backend/stocks/views.py
+++ backend/stocks/views.py
@@ -13,6 +13,15 @@ class StocksViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
+    def list(self, request):
+        """
+        method to list all the stock details added by the user.
+        """
+        stocks = models.Stocks.objects.all().filter(user=request.user)
+        serializer = serializers.StocksSerializer(stocks, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
     def create(self, request):
         """
         method to create/add a new stock and its details.
@@ -23,24 +32,18 @@ class StocksViewSet(viewsets.ModelViewSet):
 
         return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
-    def list(self, request):
-        """
-        method to list all the stock details added by the user.
-        """
-        stocks = models.Stocks.objects.all().filter(user=request.user)
-        serializer = serializers.StocksSerializer(stocks, many=True)
-
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
-
     def retrieve(self, request, pk=None):
         """
         retrieve the details of individual stock
         """
-        stock = models.Stocks.objects.get(id=pk)
+        stock = models.Stocks.objects.filter(id=pk)
         serializer = serializers.StocksSerializer(instance=stock, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def update(self, request, pk=None):
+        """
+        method for updating user stock entry
+        """
         stock = models.Stocks.objects.get(id=pk)
         serializer = serializers.StocksSerializer(instance=stock, data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -49,6 +52,9 @@ class StocksViewSet(viewsets.ModelViewSet):
         return Response(data=serializer.data, status=status.HTTP_202_ACCEPTED)
 
     def destroy(self, request, pk=None):
+        """
+        method for deleting user stock
+        """
         stock = models.Stocks.obbjects.objects.get(id=pk)
         stock.delete()
 
