a052b988deccd3bf3d7327f5f589119d8e709c42
Ebin Paul <ebinpaul@outlook.com>
2022-06-14 16:05:49 +0530

    added test for testing unauthorized stock creation

--- backend/stocks/tests.py
+++ backend/stocks/tests.py
@@ -1,7 +1,35 @@
 from django.contrib.auth import get_user_model
-from django.test import TestCase
-from stocks import models
 from django.db.utils import IntegrityError
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from stocks import models
+
+ADD_STOCK = reverse("stocks:add")
+LIST_STOCKS = reverse("stocks:list")
+
+
+def retrieve_stock_url(stock_id):
+    """
+    returns retrieve stock url with stock id
+    """
+    return reverse("stocks:retrieve", args=[stock_id])
+
+
+def update_stock_url(stock_id):
+    """
+    returns update stock url with stock id
+    """
+    return reverse("stocks:update", args=[stock_id])
+
+
+def delete_stock_url(stock_id):
+    """
+    returns delete stock url with stock id
+    """
+    return reverse("stocks:delete", args=[stock_id])
 
 
 def sample_user(email="test@gmail.com", password="testpassword"):
@@ -32,3 +60,18 @@ class StockModelTest(TestCase):
                 user=user1, stock_symbol="AAPL", company_name="Apple Inc."
             )
         self.assertEqual(IntegrityError, type(raised.exception))
+
+
+class PrivateStockAPITest(TestCase):
+    """
+    Test cases for private stock APIs
+    """
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_stock_unauthorized(self):
+        payload = {"user": 6, "stock_symbol": "TSLA", "company_name": "Tesla Inc."}
+
+        res = self.client.post(ADD_STOCK, payload)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
