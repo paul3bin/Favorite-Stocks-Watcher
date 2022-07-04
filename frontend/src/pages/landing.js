61839e7b0db9503aec978690e4e17b178efea42d
Ebin Paul <ebinpaul@outlook.com>
2022-07-04 13:16:25 +0530

    initial commit for landing

---
+++ frontend/src/pages/landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+// import Wrapper from "../components/wrapper";
+
+// import "../styles/landing.css";
+
+export function Landing() {
+  document.title = "FSW | Welcome";
+
+  return <h1>Landing Page</h1>;
+}
