bfb5bb289dc08b940721dc04fc09fe25b8c715d5
Ebin Paul <ebinpaul@outlook.com>
2022-07-09 12:08:19 +0530

    Changes to introduce form for login

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,6 +1,50 @@
 import React from "react";
 
+import "../styles/login.css";
+
 export function Login() {
   document.title = "FSW | Login";
-  return <h1>Login</h1>;
+
+  return (
+    <div className="container">
+      <div className="row justify-content-md-center">
+        <div className="card text-dark bg-light shadow p-3 mb-3 rounded">
+          <div className="text-center">
+            <main class="form-signin w-100 m-auto">
+              <form>
+                <h1 class="h3 mb-3 fw-normal">Sign In</h1>
+
+                <div class="form-floating mb-3">
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="floatingInput"
+                    placeholder="name@example.com"
+                  />
+                  <label for="floatingInput">Email address</label>
+                </div>
+
+                <div class="form-floating mb-3">
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="floatingPassword"
+                    placeholder="Password"
+                  />
+                  <label for="floatingPassword">Password</label>
+                </div>
+
+                <button class="w-100 btn btn-lg btn-primary" type="submit">
+                  Sign in
+                </button>
+                <p class="mt-5 mb-3 text-muted">
+                  Don't have an account register <a href="#">here</a>.
+                </p>
+              </form>
+            </main>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
