254019a0010458d4068c707d2cf675fb35a87cef
Ebin Paul <ebinpaul@outlook.com>
2022-07-11 19:26:32 +0530

    added proxy

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -1,6 +1,6 @@
 export class API {
   static loginUser(body) {
-    return fetch("http://localhost:8000/api/user/token", {
+    return fetch("/user/token", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
