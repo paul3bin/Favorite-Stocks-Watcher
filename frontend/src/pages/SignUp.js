aa0c7959bb51e06727c4f03adb274379c42009aa
Ebin Paul <ebinpaul@outlook.com>
2022-07-13 10:27:34 +0530

    added state managment, validation check with toastify response for fields in form

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,13 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Link } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 
+import "react-toastify/dist/ReactToastify.css";
 import "../styles/signup.css";
 
 export function SignUp() {
   document.title = "FSW | Sign-Up";
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordReEnter, setPasswordReEnter] = useState("");
+
+  const isSamePassword = () => {
+    return password === passwordReEnter;
+  };
+
+  const validEmailRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  const registerEvent = () => {
+    if (name.length !== 0) {
+      if (email.match(validEmailRegex)) {
+        if (password.length !== 0) {
+          if (password.length >= 8) {
+            if (passwordReEnter.length !== 0) {
+              if (isSamePassword) {
+                toast.success("User registered! Login to continue.", {
+                  position: toast.POSITION_TOP_RIGHT,
+                  theme: "colored",
+                });
+              } else {
+                toast.error("Passwords do not match!", {
+                  position: toast.POSITION_TOP_RIGHT,
+                  theme: "colored",
+                });
+              }
+            } else {
+              toast.error("Re-Enter password!", {
+                position: toast.POSITION_TOP_RIGHT,
+                theme: "colored",
+              });
+            }
+          } else {
+            toast.error("Password should be atleast 8 characters!", {
+              position: toast.POSITION_TOP_RIGHT,
+              theme: "colored",
+            });
+          }
+        } else {
+          toast.error("Password cannot be empty", {
+            position: toast.POSITION_TOP_RIGHT,
+            theme: "colored",
+          });
+        }
+      } else {
+        toast.error("Enter valid email!", {
+          position: toast.POSITION_TOP_RIGHT,
+          theme: "colored",
+        });
+      }
+    } else {
+      toast.error("Name field cannot be empty!", {
+        position: toast.POSITION_TOP_RIGHT,
+        theme: "colored",
+      });
+    }
+  };
+
   return (
     <div className="container">
+      <ToastContainer />
       <div className="row justify-content-md-center">
         <div className="card text-dark bg-white shadow rounded text-center signup-container">
           <main className="form-signin w-100 m-auto">
@@ -20,8 +85,8 @@ export function SignUp() {
                 id="floatingInput"
                 placeholder="Name"
                 required={true}
-                // value={email}
-                // onChange={(e) => setEmail(e.target.value)}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
               />
               <label for="floatingInput">Name</label>
             </div>
@@ -33,8 +98,8 @@ export function SignUp() {
                 id="floatingInput"
                 placeholder="name@example.com"
                 required={true}
-                // value={email}
-                // onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
               <label for="floatingInput">Email</label>
             </div>
@@ -46,8 +111,8 @@ export function SignUp() {
                 id="floatingPassword1"
                 placeholder="Password"
                 required={true}
-                // value={password}
-                // onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
               <label for="floatingPassword1">Password</label>
             </div>
@@ -59,8 +124,8 @@ export function SignUp() {
                 id="floatingPassword2"
                 placeholder="Password"
                 required={true}
-                // value={password}
-                // onChange={(e) => setPassword(e.target.value)}
+                value={passwordReEnter}
+                onChange={(e) => setPasswordReEnter(e.target.value)}
               />
               <label for="floatingPassword2">Re-enter Password</label>
             </div>
@@ -68,7 +133,7 @@ export function SignUp() {
             <button
               className="w-100 btn btn-lg btn-primary"
               type="submit"
-              // onClick={loginEvent}
+              onClick={registerEvent}
             >
               Register
             </button>
