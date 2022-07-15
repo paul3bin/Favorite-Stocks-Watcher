d5dde4b54fa0a3f9408778cfd20055c869edf20a
Ebin Paul <ebinpaul@outlook.com>
2022-07-15 11:03:45 +0530

    added functionality to return nav bar based on authentication

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 import "../styles/home.css";
 
 export function Nav() {
-  return (
+  const [cookie] = useCookies(["token"]);
+  const isAuthenticated = cookie["token"];
+
+  return isAuthenticated ? (
     <nav
       class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark "
       aria-label="Main navigation"
@@ -27,5 +31,25 @@ export function Nav() {
         </div>
       </div>
     </nav>
+  ) : (
+    <div class="container">
+      <header class="d-flex flex-wrap justify-content-center justify-content-md-between border-bottom">
+        <Link
+          to="/"
+          className="row align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"
+        >
+          <span class="fs-4">FSW</span>
+        </Link>
+
+        <div class="col-md-3 text-end">
+          <Link type="button" class="btn btn-outline-primary me-2" to="/login">
+            Login
+          </Link>
+          <Link type="button" class="btn btn-primary" to="/signup">
+            Sign-up
+          </Link>
+        </div>
+      </header>
+    </div>
   );
 }
