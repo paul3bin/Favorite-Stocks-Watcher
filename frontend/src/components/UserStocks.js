1d463838015e03393d49f5be2e28e0ebf7a1e3fb
Ebin Paul <ebinpaul@outlook.com>
2022-08-04 10:49:34 +0530

    added heading to user stocks card

--- frontend/src/components/UserStocks.js
+++ frontend/src/components/UserStocks.js
@@ -9,5 +9,10 @@ export function UserStocks(props) {
       return <StockCard stock={stock} />;
     });
 
-  return <div className="mt-5 mb-3 list-group w-auto">{stocks}</div>;
+  return (
+    <div className="mt-5 mb-3 p-3 bg-body rounded shadow">
+      <h6 className="border-bottom pb-2 mb-0">Your Stocks</h6>
+      <div className="list-group w-auto rounded">{stocks}</div>
+    </div>
+  );
 }
