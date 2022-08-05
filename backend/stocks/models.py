64d9feab9c164352e1b2c59641fec7872998ed70
Ebin Paul <ebinpaul@outlook.com>
2022-08-05 11:09:27 +0530

    added nullable constraint to stocks company name

--- backend/stocks/models.py
+++ backend/stocks/models.py
@@ -10,11 +10,11 @@ class Stocks(models.Model):
 
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=False, null=False)
     stock_symbol = models.CharField(max_length=6)
-    company_name = models.CharField(max_length=250)
+    company_name = models.CharField(max_length=250, null=True)
     added_on = models.DateTimeField(auto_now_add=True, editable=False)
 
     class Meta:
-        unique_together = ("user", "stock_symbol", "company_name")
+        unique_together = ("user", "stock_symbol")
 
     def __str__(self):
         return self.stock_symbol
