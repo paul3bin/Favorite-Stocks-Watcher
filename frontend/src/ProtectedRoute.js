7d4d3a0a0f6b0caf8b4f490fe1e115ccc0045739
Ebin Paul <ebinpaul@outlook.com>
2022-08-06 09:52:06 +0530

    changes in redirected page for non-authenticated user

--- frontend/src/ProtectedRoute.js
+++ frontend/src/ProtectedRoute.js
@@ -1,8 +1,11 @@
 import { Outlet, Navigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 
+import { PageNotFound } from "./pages/PageNotFound";
+
 export function ProtectedRoute(props) {
   const [cookies] = useCookies(["token"]);
 
-  return cookies["token"] ? <Outlet /> : <Navigate to="/login" />;
+  return cookies["token"] ? <Outlet /> : <PageNotFound />;
+  // return cookies["token"] ? <Outlet /> : <Navigate to="/login" />;
 }
