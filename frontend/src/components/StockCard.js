3352b4e4a4d7ab00a0ee4ef91947724e76b4622f
Ebin Paul <ebinpaul@outlook.com>
2022-08-08 12:17:18 +0530

    changed the stock card from cards to accordion

--- frontend/src/components/StockCard.js
+++ frontend/src/components/StockCard.js
@@ -23,66 +23,66 @@ export function StockCard(props) {
   }, []);
 
   return (
-    <div className="container mb-2">
-      <a
-        href="#"
-        className="list-group-item list-group-item-action d-flex gap-3 py-3 bg-light"
-        aria-current="true"
-        data-bs-toggle="collapse"
-        data-bs-target={`#${props.stock.stock_symbol}`}
-        aria-expanded="false"
-        aria-controls={`${props.stock.stock_symbol}`}
-      >
-        <img
-          src={profile.logo}
-          alt="twbs"
-          width="32"
-          height="32"
-          className="rounded-circle flex-shrink-0"
-        />
-        <div className="d-flex gap-2 w-100 justify-content-between">
-          <div>
-            <h6 className="mb-0">{props.stock.stock_symbol}</h6>
-            <p className="mb-0 opacity-75">{profile.name}</p>
+    <div className="accordion-item mb-2 border">
+      <h2 className="accordion-header" id="headingOne">
+        <button
+          className="accordion-button collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target={`#collapse${props.stock.stock_symbol}`}
+          aria-expanded="true"
+          aria-controls={`collapse${props.stock.stock_symbol}`}
+        >
+          <img
+            src={profile.logo}
+            alt="twbs"
+            width="32"
+            height="32"
+            className="rounded-circle flex-shrink-0"
+          />
+          <div className="d-flex gap-2 w-100 justify-content-between mx-2">
+            <div>
+              <h6 className="mb-0">{props.stock.stock_symbol}</h6>
+              <p className="mb-0 opacity-75">{profile.name}</p>
+            </div>
           </div>
-          <small className="opacity-50 text-nowrap">
-            Added on:{" "}
-            {props.stock.added_on.slice(0, 10).split("-").reverse().join("/")}
-          </small>
-        </div>
-      </a>
-      <div className="collapse" id={`${props.stock.stock_symbol}`}>
-        <div className="card card-body w-100 mb-2">
-          <div className="row">
-            <div className="col">
-              <small>Country: {profile.country}</small>
-              <br />
-              <small>Exchange: {profile.exchange}</small>
-              <br />
-              <small>IPO: {profile.ipo}</small>
-              <br />
-            </div>
-            <div className="col">
-              <small>Current Price: {stockQuote.current_price}</small>
-              <br />
-              <small>Percentage Change: {stockQuote.percent_change}</small>
-              <br />
-              <small>Opening Price: {stockQuote.opening_price}</small>
-              <br />
-              <small>
-                Previous Closing Price: {stockQuote.previous_closing_price}
-              </small>
-              <br />
-            </div>
-            <div className="col text-end">
-              <button
-                type="button"
-                className="btn btn-sm btn-outline-danger"
-                onClick={() => deleteStock(props.stock.id)}
-              >
-                Delete
-              </button>
-            </div>
+        </button>
+      </h2>
+      <div
+        id={`collapse${props.stock.stock_symbol}`}
+        className="accordion-collapse mb-2 mt-2 collapse bg-light"
+        aria-labelledby={`heading${props.stock.stock_symbol}`}
+        data-bs-parent="#accordionExample"
+      >
+        <div className="row">
+          <div className="col">
+            <small>Country: {profile.country}</small>
+            <br />
+            <small>Exchange: {profile.exchange}</small>
+            <br />
+            <small>IPO: {profile.ipo}</small>
+            <br />
+          </div>
+          <div className="col">
+            <small>Current Price: {stockQuote.current_price}</small>
+            <br />
+            <small>Percentage Change: {stockQuote.percent_change}</small>
+            <br />
+            <small>Opening Price: {stockQuote.opening_price}</small>
+            <br />
+            <small>
+              Previous Closing Price: {stockQuote.previous_closing_price}
+            </small>
+            <br />
+          </div>
+          <div className="col text-end">
+            <button
+              type="button"
+              className="btn btn-sm btn-outline-danger mt-2"
+              onClick={() => deleteStock(props.stock.id)}
+            >
+              Delete
+            </button>
           </div>
         </div>
       </div>
