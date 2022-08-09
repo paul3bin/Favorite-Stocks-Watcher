c925bba0830129a47b2383fb01079e189b068dda
Ebin Paul <ebinpaul@outlook.com>
2022-08-09 11:39:30 +0530

    minor changes

--- frontend/src/ProtectedRoute.js
+++ frontend/src/ProtectedRoute.js
@@ -6,6 +6,6 @@ import { PageNotFound } from "./pages/PageNotFound";
 export function ProtectedRoute(props) {
   const [cookies] = useCookies(["token"]);
 
-  return cookies["token"] ? <Outlet /> : <PageNotFound />;
-  // return cookies["token"] ? <Outlet /> : <Navigate to="/login" />;
+  return cookies.token ? <Outlet /> : <PageNotFound />;
+  // return cookies.token ? <Outlet /> : <Navigate to="/login" />;
 }
