d366a1a0daf7e5882c18b1ee14fd07c7accc637a
Ebin Paul <ebinpaul@outlook.com>
2022-08-11 10:36:25 +0530

    changes in core urls post disabling appending slashes in urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/user/", include("accounts.urls")),
-    path("api/stock/", include("stocks.urls")),
+    path("api/user", include("accounts.urls")),
+    path("api/stock", include("stocks.urls")),
 ]
