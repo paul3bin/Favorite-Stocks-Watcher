3b57c63b90f9fbbb6c0a0d1a076ff833d99086b8
Ebin Paul <ebinpaul@outlook.com>
2022-08-11 10:49:23 +0530

    changes in stocks urls post disabling appending slashes in urls

--- backend/stocks/urls.py
+++ backend/stocks/urls.py
@@ -1,18 +1,21 @@
 from django.urls import path
 
-from stocks.views import StocksViewSet, get_stock_quote, company_profile
+from stocks.views import (StocksViewSet, company_profile, get_stock_quote,
+                          suggested_stocks)
 
 app_name = "stocks"
 
 urlpatterns = [
     path("", StocksViewSet.as_view({"get": "list", "post": "create"}), name="stocks"),
     path(
-        "<str:pk>",
+        "/<str:pk>",
         StocksViewSet.as_view(
             {"get": "retrieve", "patch": "partial_update", "delete": "destroy"}
         ),
         name="stock",
     ),
-    path("quote/<str:stock_symbol>", get_stock_quote, name="quote"),
-    path("company-profile/<str:stock_symbol>", company_profile, name="company_profile"),
+    path("/quote/<str:stock_symbol>", get_stock_quote, name="quote"),
+    path(
+        "/company-profile/<str:stock_symbol>", company_profile, name="company_profile"
+    ),
 ]
--- backend/stocks/views.py
+++ backend/stocks/views.py
@@ -4,13 +4,10 @@ import finnhub
 from decouple import config
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
-from rest_framework import status, viewsets, request
+from rest_framework import request, status, viewsets
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.decorators import (
-    api_view,
-    authentication_classes,
-    permission_classes,
-)
+from rest_framework.decorators import (api_view, authentication_classes,
+                                       permission_classes)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -32,21 +29,26 @@ def get_stock_quote(request, stock_symbol: str) -> dict:
     o - open price of the day
     pc - previous close price
     """
+    try:
+        finnhub_client = finnhub.Client(api_key=config("FINNHUB_API_KEY"))
+        stock_quote = finnhub_client.quote(stock_symbol)
 
-    finnhub_client = finnhub.Client(api_key=config("FINNHUB_API_KEY"))
-    stock_quote = finnhub_client.quote(stock_symbol)
+        # renaming dict keys for better readability
+        stock_quote["current_price"] = stock_quote.pop("c")
+        stock_quote["change"] = stock_quote.pop("d")
+        stock_quote["percent_change"] = stock_quote.pop("dp")
+        stock_quote["high_price_of_the_day"] = stock_quote.pop("h")
+        stock_quote["low_price_of_the_day"] = stock_quote.pop("l")
+        stock_quote["opening_price"] = stock_quote.pop("o")
+        stock_quote["previous_closing_price"] = stock_quote.pop("pc")
+        stock_quote["timestamp"] = stock_quote.pop("t")
 
-    # renaming dict keys for better readability
-    stock_quote["current_price"] = stock_quote.pop("c")
-    stock_quote["change"] = stock_quote.pop("d")
-    stock_quote["percent_change"] = stock_quote.pop("dp")
-    stock_quote["high_price_of_the_day"] = stock_quote.pop("h")
-    stock_quote["low_price_of_the_day"] = stock_quote.pop("l")
-    stock_quote["opening_price"] = stock_quote.pop("o")
-    stock_quote["previous_closing_price"] = stock_quote.pop("pc")
-    stock_quote["timestamp"] = stock_quote.pop("t")
+        return Response(stock_quote)
 
-    return Response(stock_quote)
+    except:
+        return Response(
+            {"message": "Something went wrong"}, status=status.HTTP_400_BAD_REQUEST
+        )
 
 
 @api_view(["GET"])
@@ -56,12 +58,37 @@ def company_profile(request, stock_symbol: str) -> dict:
     """
     Function that receives a stock symbol and returns a dictionary containing the company profile
     """
-    finnhub_client = finnhub.Client(api_key=config("FINNHUB_API_KEY"))
-    profile = finnhub_client.company_profile2(symbol=stock_symbol)
+    try:
+        finnhub_client = finnhub.Client(api_key=config("FINNHUB_API_KEY"))
+        profile = finnhub_client.company_profile2(symbol=stock_symbol)
 
-    finnhub_client.close()
+        finnhub_client.close()
 
-    return Response(profile)
+        return Response(profile, status=status.HTTP_200_OK)
+
+    except:
+        return Response(
+            {"message": "Something went wrong"}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+@api_view(["GET"])
+@permission_classes((IsAuthenticated,))
+@authentication_classes((TokenAuthentication,))
+@cache_page(60 * 2)
+def suggested_stocks(request, exchange):
+    try:
+        finnhub_client = finnhub.Client(api_key=config("FINNHUB_API_KEY"))
+        stocks = finnhub_client.stock_symbols("US")
+
+        finnhub_client.close()
+
+        return Response(stocks, status=status.HTTP_200_OK)
+
+    except:
+        return Response(
+            {"message": "Something went wrong"}, status=status.HTTP_400_BAD_REQUEST
+        )
 
 
 class StocksViewSet(viewsets.ModelViewSet, viewsets.ViewSet):
