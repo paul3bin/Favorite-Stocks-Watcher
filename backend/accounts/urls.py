0c06cdeda0cd5c0da605671c4911f67463c4b82d
Ebin Paul <ebinpaul@outlook.com>
2022-08-11 10:35:47 +0530

    changes in accounts URLs post disabling appending slashes in URLs

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -6,7 +6,9 @@ app_name = "accounts"
 
 urlpatterns = [
     path("", views.CreateUserView.as_view(), name="create_user"),
-    path("token", views.CreateTokenView.as_view(), name="get_token"),
-    path("me", views.ManageUserView.as_view(), name="manage_user"),
-    path("change-password", views.ChangePasswordView.as_view(), name="change-password"),
+    path("/token", views.CreateTokenView.as_view(), name="get_token"),
+    path("/me", views.ManageUserView.as_view(), name="manage_user"),
+    path(
+        "/change-password", views.ChangePasswordView.as_view(), name="change-password"
+    ),
 ]
